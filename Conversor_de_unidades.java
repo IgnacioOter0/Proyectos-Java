e28a88334b8857d4be3a60a4909a92206c24409b
IgnacioOtero <ioterocapo@gmail.com>
2025-04-25 01:48:36 -0300

    Update Conversor_de_unidades.java

--- Conversor_de_unidades.java
+++ Conversor_de_unidades.java
@@ -4,12 +4,6 @@ import java.util.Scanner;
 
 public class Conversor_de_unidades {
 
-    /*
-    Crear un programa que convierta entre diferentes unidades:
-    Temperatura (Celsius, Fahrenheit, Kelvin).
-    Distancia (metros, kilómetros, millas).
-    Peso (kilogramos, libras).
-     */
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -25,33 +19,30 @@ public class Conversor_de_unidades {
         System.out.println("3) Peso");
         System.out.println("4) Salir del programa");
 
-        int valorMenu1 = 0;
-        while (valorMenu1 != 4) {
-            valorMenu1 = sc.nextInt();
-            switch (valorMenu1) {
+        int valorMenu1;
+        valorMenu1 = sc.nextInt();
+        switch (valorMenu1) {
 
-                case 1:
-                    convertirTemperatura(sc);
-                    break;
+            case 1:
+                convertirTemperatura(sc);
+                break;
 
-                case 2:
-                    convertirDistancia(sc);
-                    break;
+            case 2:
+                convertirDistancia(sc);
+                break;
 
-                case 3:
-                    convertirPeso(sc);
-                    break;
+            case 3:
+                convertirPeso(sc);
+                break;
 
-                case 4:
-                    break;
+            case 4:
+                break;
 
-                default:
-                    System.out.println("Ingrese una opción válida");
-
-            }
+            default:
+                System.out.println("Ingrese una opción válida");
+                menuPrincipal(sc);
 
         }
-
     }
 
     public static void convertirTemperatura(Scanner sc) {
@@ -63,10 +54,16 @@ public class Conversor_de_unidades {
         System.out.println("4) Atrás");
         System.out.println("5) Salir del programa");
 
-        int valorMenu2 = 0;
-        int unidadDestino;
+        int valorMenu2;
+        int unidadDestino = 0;
         valorMenu2 = sc.nextInt();
         int unidadOrigen = valorMenu2;
+
+        convertirTemperatura2(sc, valorMenu2, unidadDestino, unidadOrigen);
+    }
+
+    public static void convertirTemperatura2(Scanner sc, int valorMenu2, int unidadDestino, int unidadOrigen) {
+
         switch (valorMenu2) {
             case 1:
                 System.out.println("Elija la unidad a la cual convertir");
@@ -75,7 +72,7 @@ public class Conversor_de_unidades {
                 System.out.println("3) Atrás");
                 System.out.println("4) Salir del programa");
                 unidadDestino = sc.nextInt();
-                convertirTemperatura2(sc, unidadOrigen, unidadDestino);
+                convertirTemperatura3(sc, valorMenu2, unidadOrigen, unidadDestino);
                 break;
 
             case 2:
@@ -85,7 +82,7 @@ public class Conversor_de_unidades {
                 System.out.println("3) Atrás");
                 System.out.println("4) Salir del programa");
                 unidadDestino = sc.nextInt();
-                convertirTemperatura2(sc, unidadOrigen, unidadDestino);
+                convertirTemperatura3(sc, valorMenu2, unidadOrigen, unidadDestino);
                 break;
 
             case 3:
@@ -95,37 +92,55 @@ public class Conversor_de_unidades {
                 System.out.println("3) Atrás");
                 System.out.println("4) Salir del programa");
                 unidadDestino = sc.nextInt();
-                convertirTemperatura2(sc, unidadOrigen, unidadDestino);
+                convertirTemperatura3(sc, valorMenu2, unidadOrigen, unidadDestino);
                 break;
 
             case 4:
-                System.out.println("Elija el tipo de conversión:");
-                System.out.println("1) Temperatura");
-                System.out.println("2) Distancia");
-                System.out.println("3) Peso");
-                System.out.println("4) Salir del programa");
-                return;
+                menuPrincipal(sc);
+                break;
 
             case 5:
                 System.exit(0);
-                break;
 
             default:
                 System.out.println("Ingrese una opción válida");
-                return;
+                convertirTemperatura(sc);
+                break;
         }
     }
 
-    public static void convertirTemperatura2(Scanner sc, int unidadOrigen, int unidadDestino) {
+    public static void convertirTemperatura3(Scanner sc, int valorMenu2, int unidadOrigen, int unidadDestino) {
 
-        double cantidadConvertir = 0;
+        double cantidadConvertir;
+        switch (unidadDestino) {
+            case 1:
+                System.out.println("Escriba la cantidad que desea convertir");
+                cantidadConvertir = sc.nextDouble();
+                convertirTemperatura4(sc, cantidadConvertir, unidadOrigen, unidadDestino);
+                break;
 
-        System.out.println("Escriba la cantidad que desea convertir");
-        cantidadConvertir = sc.nextDouble();
-        convertirTemperatura3(sc, cantidadConvertir, unidadOrigen, unidadDestino);
+            case 2:
+                System.out.println("Escriba la cantidad que desea convertir");
+                cantidadConvertir = sc.nextDouble();
+                convertirTemperatura4(sc, cantidadConvertir, unidadOrigen, unidadDestino);
+                break;
+
+            case 3:
+                convertirTemperatura(sc);
+                break;
+
+            case 4:
+                System.exit(0);
+
+            default:
+                System.out.println("Ingrese una opción válida");
+                convertirTemperatura2(sc, valorMenu2, unidadDestino, unidadOrigen);
+                break;
+
+        }
     }
 
-    public static void convertirTemperatura3(Scanner sc, double cantidadConvertir, int unidadOrigen, int unidadDestino) {
+    public static void convertirTemperatura4(Scanner sc, double cantidadConvertir, int unidadOrigen, int unidadDestino) {
 
         double resultadoFinal = 0;
         switch (unidadOrigen) {
@@ -154,27 +169,301 @@ public class Conversor_de_unidades {
                 break;
 
             default:
-                System.out.println("ingrese una opción válida");
+                System.out.println("Ingrese una opción válida");
                 return;
         }
 
-        System.out.println("El resultado es: " + resultadoFinal);
+        if (resultadoFinal % 1 == 0) {
+            System.out.printf("El resultado es: %.0f\n", resultadoFinal);
+        } else {
+            System.out.printf("El resultado es: %.4f\n", resultadoFinal);
+        }
+        reset(sc);
     }
 
     public static void convertirDistancia(Scanner sc) {
+
         System.out.println("Elija la unidad para convertir");
-        System.out.println("1) Metros");
-        System.out.println("2) Kilómetros");
-        System.out.println("3) Millas");
+        System.out.println("1) Kilómetros");
+        System.out.println("2) Millas");
+        System.out.println("3) Pies");
         System.out.println("4) Atrás");
         System.out.println("5) Salir del programa");
+
+        int valorMenu2;
+        int unidadDestino = 0;
+        valorMenu2 = sc.nextInt();
+        int unidadOrigen = valorMenu2;
+
+        convertirDistancia2(sc, valorMenu2, unidadDestino, unidadOrigen);
+    }
+
+    public static void convertirDistancia2(Scanner sc, int valorMenu2, int unidadDestino, int unidadOrigen) {
+
+        switch (valorMenu2) {
+            case 1:
+                System.out.println("Elija la unidad a la cual convertir");
+                System.out.println("1) Millas");
+                System.out.println("2) Pies");
+                System.out.println("3) Atrás");
+                System.out.println("4) Salir del programa");
+                unidadDestino = sc.nextInt();
+                convertirDistancia3(sc, valorMenu2, unidadOrigen, unidadDestino);
+                break;
+
+            case 2:
+                System.out.println("Elija la unidad a la cual convertir");
+                System.out.println("1) Kilómetros");
+                System.out.println("2) Pies");
+                System.out.println("3) Atrás");
+                System.out.println("4) Salir del programa");
+                unidadDestino = sc.nextInt();
+                convertirDistancia3(sc, valorMenu2, unidadOrigen, unidadDestino);
+                break;
+
+            case 3:
+                System.out.println("Elija la unidad a la cual convertir");
+                System.out.println("1) Kilómetros");
+                System.out.println("2) Millas");
+                System.out.println("3) Atrás");
+                System.out.println("4) Salir del programa");
+                unidadDestino = sc.nextInt();
+                convertirDistancia3(sc, valorMenu2, unidadOrigen, unidadDestino);
+                break;
+
+            case 4:
+                menuPrincipal(sc);
+                break;
+
+            case 5:
+                System.exit(0);
+
+            default:
+                System.out.println("Ingrese una opción válida");
+                convertirDistancia(sc);
+                break;
+        }
+    }
+
+    public static void convertirDistancia3(Scanner sc, int valorMenu2, int unidadOrigen, int unidadDestino) {
+
+        double cantidadConvertir;
+        switch (unidadDestino) {
+            case 1:
+                System.out.println("Escriba la cantidad que desea convertir");
+                cantidadConvertir = sc.nextDouble();
+                convertirDistancia4(sc, cantidadConvertir, unidadOrigen, unidadDestino);
+                break;
+
+            case 2:
+                System.out.println("Escriba la cantidad que desea convertir");
+                cantidadConvertir = sc.nextDouble();
+                convertirDistancia4(sc, cantidadConvertir, unidadOrigen, unidadDestino);
+                break;
+
+            case 3:
+                convertirDistancia(sc);
+                break;
+
+            case 4:
+                System.exit(0);
+
+            default:
+                System.out.println("Ingrese una opción válida");
+                convertirDistancia2(sc, valorMenu2, unidadDestino, unidadOrigen);
+                break;
+
+        }
+    }
+
+    public static void convertirDistancia4(Scanner sc, double cantidadConvertir, int unidadOrigen, int unidadDestino) {
+
+        double resultadoFinal = 0;
+        switch (unidadOrigen) {
+            case 1: // Kilómetros
+                if (unidadDestino == 1) {
+                    resultadoFinal = cantidadConvertir * 0.6213711922; // Millas
+                } else if (unidadDestino == 2) {
+                    resultadoFinal = cantidadConvertir * 3280.839895; // Pies
+                }
+                break;
+
+            case 2: // Millas
+                if (unidadDestino == 1) {
+                    resultadoFinal = cantidadConvertir * 1.609344; // Kilómetros
+                } else if (unidadDestino == 2) {
+                    resultadoFinal = cantidadConvertir * 5280; // Pies
+                }
+                break;
+
+            case 3: // Pies
+                if (unidadDestino == 1) {
+                    resultadoFinal = cantidadConvertir / 3280.839895; // Kilómetros
+                } else if (unidadDestino == 2) {
+                    resultadoFinal = cantidadConvertir / 5280; // Millas
+                }
+                break;
+        }
+
+        if (resultadoFinal % 1 == 0) {
+            System.out.printf("El resultado es: %.0f\n", resultadoFinal);
+        } else {
+            System.out.printf("El resultado es: %.4f\n", resultadoFinal);
+        }
+        reset(sc);
     }
 
     public static void convertirPeso(Scanner sc) {
+
         System.out.println("Elija la unidad para convertir");
         System.out.println("1) Kilogramos");
         System.out.println("2) Libras");
-        System.out.println("3) Atrás");
-        System.out.println("4) Salir del programa");
+        System.out.println("3) Onzas");
+        System.out.println("4) Atrás");
+        System.out.println("5) Salir del programa");
+
+        int valorMenu2;
+        int unidadDestino = 0;
+        valorMenu2 = sc.nextInt();
+        int unidadOrigen = valorMenu2;
+
+        convertirPeso2(sc, valorMenu2, unidadDestino, unidadOrigen);
+    }
+
+    public static void convertirPeso2(Scanner sc, int valorMenu2, int unidadDestino, int unidadOrigen) {
+
+        switch (valorMenu2) {
+            case 1:
+                System.out.println("Elija la unidad a la cual convertir");
+                System.out.println("1) Libras");
+                System.out.println("2) Onzas");
+                System.out.println("3) Atrás");
+                System.out.println("4) Salir del programa");
+                unidadDestino = sc.nextInt();
+                convertirPeso3(sc, valorMenu2, unidadOrigen, unidadDestino);
+                break;
+
+            case 2:
+                System.out.println("Elija la unidad a la cual convertir");
+                System.out.println("1) Kilogramos");
+                System.out.println("2) Onzas");
+                System.out.println("3) Atrás");
+                System.out.println("4) Salir del programa");
+                unidadDestino = sc.nextInt();
+                convertirPeso3(sc, valorMenu2, unidadOrigen, unidadDestino);
+                break;
+
+            case 3:
+                System.out.println("Elija la unidad a la cual convertir");
+                System.out.println("1) Kilogramos");
+                System.out.println("2) Libras");
+                System.out.println("3) Atrás");
+                System.out.println("4) Salir del programa");
+                unidadDestino = sc.nextInt();
+                convertirPeso3(sc, valorMenu2, unidadOrigen, unidadDestino);
+                break;
+
+            case 4:
+                menuPrincipal(sc);
+                break;
+
+            case 5:
+                System.exit(0);
+
+            default:
+                System.out.println("Ingrese una opción válida");
+                convertirPeso(sc);
+                break;
+        }
+    }
+
+    public static void convertirPeso3(Scanner sc, int valorMenu2, int unidadOrigen, int unidadDestino) {
+
+        double cantidadConvertir;
+        switch (unidadDestino) {
+            case 1:
+                System.out.println("Escriba la cantidad que desea convertir");
+                cantidadConvertir = sc.nextDouble();
+                convertirPeso4(sc, cantidadConvertir, unidadOrigen, unidadDestino);
+                break;
+
+            case 2:
+                System.out.println("Escriba la cantidad que desea convertir");
+                cantidadConvertir = sc.nextDouble();
+                convertirPeso4(sc, cantidadConvertir, unidadOrigen, unidadDestino);
+                break;
+
+            case 3:
+                convertirPeso(sc);
+                break;
+
+            case 4:
+                System.exit(0);
+
+            default:
+                System.out.println("Ingrese una opción válida");
+                convertirPeso2(sc, valorMenu2, unidadDestino, unidadOrigen);
+                break;
+
+        }
+    }
+
+    public static void convertirPeso4(Scanner sc, double cantidadConvertir, int unidadOrigen, int unidadDestino) {
+
+        double resultadoFinal = 0;
+        switch (unidadOrigen) {
+            case 1: // Kilogramos
+                if (unidadDestino == 1) {
+                    resultadoFinal = cantidadConvertir * 2.2046226218; // Libras
+                } else if (unidadDestino == 2) {
+                    resultadoFinal = cantidadConvertir * 35.27396195; // Onzas
+                }
+                break;
+
+            case 2: // Libras
+                if (unidadDestino == 1) {
+                    resultadoFinal = cantidadConvertir * 0.45359237; // Kilogramos
+                } else if (unidadDestino == 2) {
+                    resultadoFinal = cantidadConvertir * 16; // Onzas
+                }
+                break;
+
+            case 3: // Onzas
+                if (unidadDestino == 1) {
+                    resultadoFinal = cantidadConvertir * 0.0283495231; // Kilogramos
+                } else if (unidadDestino == 2) {
+                    resultadoFinal = cantidadConvertir / 16; // Libras
+                }
+                break;
+        }
+
+        if (resultadoFinal % 1 == 0) {
+            System.out.printf("El resultado es: %.0f\n", resultadoFinal);
+        } else {
+            System.out.printf("El resultado es: %.4f\n", resultadoFinal);
+        }
+        reset(sc);
+    }
+
+    public static void reset(Scanner sc) {
+
+        System.out.println("¿Querés realizar otra conversión?");
+        System.out.println("1) Si");
+        System.out.println("2) No");
+
+        int valorMenu0 = sc.nextInt();
+
+        switch (valorMenu0) {
+            case 1:
+                menuPrincipal(sc);
+
+            case 2:
+                System.exit(0);
+
+            default:
+                System.out.println("Ingrese una opción válida");
+                reset(sc);
+
+        }
     }
 }
